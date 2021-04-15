f8583084cf229d4a1c1aab5b1748ca358e45d2ea
richard lechner <richard@richardlechner.de>
2021-04-15 18:47:17 +0200

    included swagger and swagger-ui

--- NewCbudgetServerNew/src/main/java/de/lechner/cbudgetserver/NewCbudgetServerNewApplication.java
+++ NewCbudgetServerNew/src/main/java/de/lechner/cbudgetserver/NewCbudgetServerNewApplication.java
@@ -3,7 +3,10 @@ package de.lechner.cbudgetserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class NewCbudgetServerNewApplication {
 
 	public static void main(String[] args) {
