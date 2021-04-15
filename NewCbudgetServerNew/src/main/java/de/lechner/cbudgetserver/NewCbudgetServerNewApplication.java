4a857898c1f0eadc295a22679d07fd695e78f0aa
richard lechner <richard@richardlechner.de>
2021-04-15 21:36:46 +0200

    Docket

--- NewCbudgetServerNew/src/main/java/de/lechner/cbudgetserver/NewCbudgetServerNewApplication.java
+++ NewCbudgetServerNew/src/main/java/de/lechner/cbudgetserver/NewCbudgetServerNewApplication.java
@@ -2,7 +2,10 @@ package de.lechner.cbudgetserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
@@ -11,6 +14,12 @@ public class NewCbudgetServerNewApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NewCbudgetServerNewApplication.class, args);
+	}
+	
+	@Bean
+	public Docket swaggerConfiguration() {
+		
+		return (new Docket(DocumentationType.SWAGGER_2 ));
 	}
 
 }
