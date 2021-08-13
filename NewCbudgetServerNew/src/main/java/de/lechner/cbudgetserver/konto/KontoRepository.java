3b247a854d9d0300a2f10eb3fc1127097d97dbb6
richard lechner <richard@richardlechner.de>
2021-08-13 20:13:03 +0200

    test

--- NewCbudgetServerNew/src/main/java/de/lechner/cbudgetserver/konto/KontoRepository.java
+++ NewCbudgetServerNew/src/main/java/de/lechner/cbudgetserver/konto/KontoRepository.java
@@ -3,7 +3,7 @@ package de.lechner.cbudgetserver.konto;
 import org.springframework.data.repository.CrudRepository;
 
 public interface KontoRepository extends CrudRepository<Konto, Integer> {
-
+   
 	}
 
 
