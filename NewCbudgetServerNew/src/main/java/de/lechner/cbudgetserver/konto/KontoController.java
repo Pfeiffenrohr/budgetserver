35ddb5768f45de382ec7b80203d825475a31f491
richard lechner <richard@richardlechner.de>
2026-05-05 18:39:55 +0200

    KontoByMode muss eine Liste von Kontos zurück geben 2

--- NewCbudgetServerNew/src/main/java/de/lechner/cbudgetserver/konto/KontoController.java
+++ NewCbudgetServerNew/src/main/java/de/lechner/cbudgetserver/konto/KontoController.java
@@ -63,15 +63,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 	         }   
 		 }
 
-		@RequestMapping("/kontoByAnlageart/{name}")
-		public ResponseEntity<Integer> getkontobyMode(@PathVariable("name") String name) {
-			List <Konto> kontos= kontoservice.getKontoByMode(name);
-			{
-				return new ResponseEntity<>(0, HttpStatus.NOT_FOUND);
-			}
+		@RequestMapping("/kontoByAnlageart/{mode}")
+		public List <Konto> getKontoByAnlageart(@PathVariable("mode") String mode) {
+			return kontoservice.getKontoByMode(mode);
 		}
-		 
-		 
 		 
 		 @RequestMapping(method=RequestMethod.POST, value="/kontos")
 		 public void addKonto(@RequestBody Konto konto) {
